cd23492ec601a0c59fe469f6b117b01a56a6b2fb
Mikssxed <112573426+Mikssxed@users.noreply.github.com>
2022-12-02 18:17:51 +0100

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,63 @@
-const section = document.querySelectorAll("section");
-const dot = document.querySelectorAll(".dot");
-const markAll = document.querySelector("#read-all");
-const number = document.querySelector("#number");
-const toggleRead = (nr) => {
-    section[nr].classList.toggle("unread");
-    dot[nr].classList.toggle("dotOn");
-};
-const markAllFunc = () => {
-    for (let i = 0; i < 7; i++) {
-        section[i].classList.remove("unread");
-        dot[i].classList.remove("dotOn");
+const inputNumber = document.querySelectorAll('input[type="number"]');
+const inputName = document.querySelector('input[type="text"]');
+const inputCardNumber = document.getElementById('cardNumber');
+let i = 0;
+const allInputs = document.querySelectorAll('input');
+const allP = document.querySelectorAll('p');
+const button = document.querySelector('#submit');
+const number = document.getElementById('number');
+const chName = document.getElementById('name');
+const date = document.getElementById('date');
+const cvc = document.getElementById('cvc');
+const month = document.getElementById('cardMonth');
+const year = document.getElementById('cardYear');
+const form = document.querySelector('form');
+const completed = document.getElementById('completed')
+
+const freeSpace = () => {
+    if (inputCardNumber.value.length < 17 && inputCardNumber.value.length > 0 && inputCardNumber.value.length % 4 == i) {
+        inputCardNumber.value += " ";
+        i++;
+    } else if (inputCardNumber.value.length > 17){
+        i = 0;
     }
-    number.innerHTML = "0";
-};
-const numberCount = () => {
-    let counter = 0;
-    for (let i = 0; i < 7; i++) {
-        let currentSection = section[i];
-        if (currentSection.classList.contains("unread")) {
-            counter++;
+}
+
+const validate = () => {
+    for (let i = 0; i <= 4; i++){
+        if (allInputs[i].value == '') {
+            allP[i].innerHTML = "Can't be blank";
+            allInputs[i].style.border = '1px solid var(--primary-color2)';
+        } else if (/\d/.test(allInputs[1].value) == false) {
+            allP[1].innerHTML = "Wrong format, numbers only";
+        } else {
+            allP[i].innerHTML = "";
+            allInputs[i].style.border = '1px solid var(--neutral-color2)';
         }
     }
-    number.innerHTML = String(counter);
-};
-window.addEventListener("click", numberCount);
-markAll.addEventListener("click", markAllFunc);
+}
+const transfer1 = () => chName.innerHTML = allInputs[0].value;
+const transfer2 = () => number.innerHTML = inputCardNumber.value;
+const transfer3 = () => month.innerHTML = allInputs[2].value;
+const transfer4 = () => year.innerHTML = allInputs[3].value;
+const transfer5 = () => cvc.innerHTML = allInputs[4].value;
+
+const submit = () => {
+    if (allInputs[0].style.border !== '1px solid var(--primary-color2)'&&
+    allInputs[1].style.border !== '1px solid var(--primary-color2)'&&
+    allInputs[2].style.border !== '1px solid var(--primary-color2)'&&
+    allInputs[3].style.border !== '1px solid var(--primary-color2)'&&
+    allInputs[4].style.border !== '1px solid var(--primary-color2)') {
+        form.style.display = 'none';
+        completed.style.display = 'flex';
+    }
+    
+}
+inputCardNumber.addEventListener('keydown', freeSpace);
+button.addEventListener('click', validate);
+inputCardNumber.addEventListener('keyup', transfer2);
+allInputs[0].addEventListener('keyup', transfer1);
+allInputs[2].addEventListener('keyup', transfer3);
+allInputs[3].addEventListener('keyup', transfer4);
+allInputs[4].addEventListener('keyup', transfer5);
+button.addEventListener('click', submit);
